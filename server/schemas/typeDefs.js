f26999a7ee86935f5f43a1a4370833695c2f3a82
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-12 18:17:21 -0500

    typedefs are finished. Not sure if other mutations are required

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,35 @@
+const typeDefs = `
+  type  User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
+
+  type Book {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    user: User
+    book: Book
+    books: [Book]
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookId: ID!): Book
+  }
+`
+module.exports = typeDefs;
